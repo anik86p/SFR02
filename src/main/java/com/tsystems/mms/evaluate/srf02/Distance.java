1790524780b82e6f2de9051e2dbf5f4e4d7267e3
anik <anikwork86p@gmail.com>
2020-04-20 17:01:05 +0200

    done

--- src/main/java/com/tsystems/mms/evaluate/srf02/Distance.java
+++ src/main/java/com/tsystems/mms/evaluate/srf02/Distance.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Distance {
+	public int x=10;
 
 	public static final String UNIT_MM = "mm";
 	public static final String UNIT_CM = "cm";
